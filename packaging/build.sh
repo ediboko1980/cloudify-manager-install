2fa48f01b1426873176ee1e97da91e8fe3cb1958
Łukasz Maksymczuk <lukasz@lukasz-maksymczuk.intra.codilime.com>
2019-10-29 13:47:07 +0100

    wip

--- packaging/build.sh
+++ packaging/build.sh
@@ -3,12 +3,13 @@ mkdir ~/.ssh
 cp /tmp/cloudify-manager-install/premium.pem ~/.ssh/id_rsa
 chmod 0400 ~/.ssh/id_rsa
 ssh-keyscan github.com >> ~/.ssh/known_hosts
-pushd /tmp/cloudify-manager-install/rpms
-    /tmp/cloudify-manager-install/packaging/fetch_requirements --edition premium
-popd
 rm ~/rpm -fr
-ln -s /tmp/cloudify-manager-install ~/rpm
-pushd /tmp/cloudify-manager-install
+cp /tmp/cloudify-manager-install ~/rpm
+pushd ~/rpm/rpms
+    ~/rpm/packaging/fetch_requirements --edition premium
+popd
+pushd ~/rpm
     source packaging/version_info
     rpmbuild -D "CLOUDIFY_VERSION ${CLOUDIFY_VERSION}" -D "CLOUDIFY_PACKAGE_RELEASE ${CLOUDIFY_PACKAGE_RELEASE}" -bb packaging/install_rpm.spec
+    cp x86_64/*.rpm /tmp/cloudify-manager-install 
 popd
